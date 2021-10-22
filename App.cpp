5f61ae0f9d68aaf4a2c7ed50b927975bbb2dbcff
Nc Galih <encegalih@gmail.com>
2021-10-22 21:43:07 +0700

    fix bug

--- App.cpp
+++ App.cpp
@@ -16,7 +16,7 @@ string weekday(int d){
     switch (d){
     case 1: return "Senin"; case 2: return "Selasa"; case 3: return "Rabu" ;
     case 4: return "Kamis"; case 5: return "Jumat" ; case 6: return "Sabtu";
-    case 7: return "Minggu";
+    case 7: return "Minggu"; case 0: return "Minggu";
     }
 }
 
